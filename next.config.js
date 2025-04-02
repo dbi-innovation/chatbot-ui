8efd5bae09d5434401a8646871d5d3b06d70b8d2
Andrew <aphinan_th@kkumail.com>
2025-04-02 13:59:48 +0700

    feat: configure Next.js for standalone output in next.config.js

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ module.exports = withBundleAnalyzer(
     experimental: {
       serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
     },
+    output: "standalone",
     webpack: (config, { isServer, webpack }) => {
       if (isServer) {
         config.externals.push({ "nodejs-polars": "commonjs nodejs-polars" })
